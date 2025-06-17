28227159bc4cd76b96dc065eb4a55e92506c211c
wowobaldet <163464406+wowobaldet@users.noreply.github.com>
2025-06-17 08:45:45 +0700

    Delete backend directory

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
